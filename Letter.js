34e79e8811b983c94a0549fd0a0b5ed316195a8b
Jaime <jaimebmorin@gmail.com>
2018-02-01 22:25:05 -0600

    working on 1st constructor

---
+++ Letter.js
@@ -0,0 +1,16 @@
+
+
+function Letter(character, guessed) {
+    this.character = "2";
+    this.guessed = false;
+}
+
+Letter.prototype.displayCharacter = function () {
+
+
+}
+
+Letter.prototype.checkGuess = function () {
+    
+
+}
