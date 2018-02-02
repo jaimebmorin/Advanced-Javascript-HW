207c9f00a40622fe761688d9d8af18575b4946c6
Jaime <jaimebmorin@gmail.com>
2018-02-01 22:58:33 -0600

    letter.js is working

--- Letter.js
+++ Letter.js
@@ -1,16 +1,25 @@
-
+var guessInput = "r";
+var letter = new Letter("r");
 
 function Letter(character, guessed) {
-    this.character = "2";
-    this.guessed = false;
+    this.character = character;
+    this.guessed = guessed || false;
+    this.displayCharacter = function () {
+        if (this.guessed) {
+            console.log(this.character);
+        } else {
+            console.log("_");
+        }
+    }
+    this.CheckGuess = function(guess) {
+        if (guess === this.character) {
+            this.guessed = true; 
+            this.displayCharacter();
+        } else {
+        this.displayCharacter();
+        }
+    }
 }
 
-Letter.prototype.displayCharacter = function () {
 
-
-}
-
-Letter.prototype.checkGuess = function () {
-    
-
-}
+letter.CheckGuess(guessInput);
